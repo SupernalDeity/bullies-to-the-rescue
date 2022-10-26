fd4c729a67aae1655a7576b446f0116012f2f8fa
Jennifer Arnold <canna@macbook-pro.lan>
2022-10-26 14:18:38 -0400

    homepage template

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,6 +6,16 @@ import Auth from '../utils/auth';
 import { QUERY_USERS } from '../utils/queries';
 // Components
 import UserList from '../components/UserList';
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+]
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_USERS);
@@ -25,14 +35,57 @@ const Home = () => {
   }
 
   return (
-    <main>
-      <div>
-        {renderUsername()}
+    <div className="relative overflow-hidden bg-white">
+      <div className="mx-auto max-w-7xl">
+        <div className="relative z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
+          <svg
+            className="absolute inset-y-0 right-0 hidden h-full w-48 translate-x-1/2 transform text-white lg:block"
+            fill="currentColor"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            aria-hidden="true"
+          >
+            <polygon points="50,0 100,0 50,100 0,100" />
+          </svg>
+
+          
+                      
+
+          <main className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+            <div className="sm:text-center lg:text-left">
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+                <span className="block xl:inline">Bullies</span>{' '}
+                <span className="block text-indigo-600 xl:inline">to the Rescue</span>
+              </h1>
+              <p className="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
+                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+                fugiat veniam occaecat fugiat aliqua.
+              </p>
+              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                <div className="rounded-md shadow">
+                  <a
+                    href="#"
+                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg"
+                  >
+                    Get started
+                  </a>
+                </div>
+                <div className="mt-3 sm:mt-0 sm:ml-3">
+                 
+                </div>
+              </div>
+            </div>
+          </main>
+        </div>
       </div>
-      <div>
-        {renderUserList()}
+      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <img
+          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full"
+          src="https://images.ctfassets.net/sfnkq8lmu5d7/45un3KmPuTLyUNDwDUOkh7/b17ba9b45cdec97c257be30e18517ec2/2022-05-18_English_Bulldogs_Face_Extinction_Stocksy_txp680f3471PHQ300_Medium_192223.jpg?w=1000&h=750&fl=progressive&q=80&fm=jpg"
+          alt="BullDog"
+        />
       </div>
-    </main>
+    </div>
   );
 };
 
