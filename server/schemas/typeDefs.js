243afa1f1d13b717f22c6538fb4695c4d7ddf6f4
Anthony <perezant121@gmail.com>
2022-10-26 10:49:27 -0400

    application typedef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,6 +13,100 @@ const typeDefs = gql`
     user: User
   }
 
+  type Application {
+    _id: ID
+    name: String 
+    age: Int
+    coapplicant: String
+    coapplicantage: Int
+    address: String 
+    email: String 
+    phone: String 
+    housemembers: String 
+    occupation: String 
+    employerinfo: String 
+    coappoccupation: String 
+    coappemployerinfo: String
+    ownbulldog: Boolean
+    medicalissues: Boolean
+    otherissues: String 
+    dogbehavior: Boolean
+    dwellinginfo: Boolean
+    landlord: Landlord
+    fencedyard: String
+    stairs: String 
+    water: String 
+    airconditioning: Airconditioning
+    wheredog: Wheredog
+    allergy: Boolean
+    family: Family
+    age: String
+    currentpets: String
+    medical: String
+    adoptwhen: String
+    surrenderpet: String
+    adoptedanimal: String
+    animalfriends: String
+    bite: String 
+    heartworm: String 
+    vetcare: String 
+    vetinfo: Vetinfo
+    references: String 
+    behavior: String 
+    correction: String 
+    crates: String 
+    trainingissues: String 
+    commonissues: String 
+    questionare: Questionaire
+  }  
+
+  type Landlord {
+    name: String
+    number: String
+  }
+
+  type Airconditioning {
+    home: Boolean
+    car: Boolean
+  }
+
+  type Wheredog {
+    day: String 
+    night: String
+  }
+
+  type Family {
+    willingness: String
+    why: String
+    children: String 
+  }
+
+  type vetinfo {
+    office: String
+    name: String
+    address: String 
+    number: String 
+  }
+
+  type Questionaire {
+    heat: Boolean
+    food: Boolean
+    palate: Boolean
+    allergy: Boolean
+    spayneuter: String 
+    proccessing: Boolean
+    hearsay: String 
+    previouspurchase: String 
+    location: Boolean 
+    appfee: Boolean
+    appvalid: Boolean
+    commitment: Boolean
+    donate: Boolean
+    accuracy: Boolean
+    comments: String 
+    date: String 
+  }
+
   type Query {
     users: [User]
     user(id: ID!): User
