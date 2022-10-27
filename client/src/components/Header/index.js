e4ff48574e7dff98b427dad6ed2e75f6ac6967b6
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-10-27 12:24:19 -0400

    navbar links

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -6,12 +6,10 @@ const Header = () => {
   return (
     <header>
       <div>
-        <Link to="/">
-          <h1>User List</h1>
-        </Link>
+       
       </div>
       <div>
-        <p>Simple App to View Users.</p>
+       
         <Navbar />
       </div>
     </header>
