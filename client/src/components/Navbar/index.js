59cf772fb93a013a2a9c63d9375193fddd410db9
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-10-27 13:08:38 -0400

    added navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -161,10 +161,12 @@ function Navbar() {
           </div>
         </div>
 
+        <Disclosure.Panel className={"sm:hidden"}>
           <div className="space-y-1 px-2 pt-2 pb-3">
             {navigation.map((item) => 
-              <a
+              <Disclosure.Button
               key={item.name}
+              as='a'
               href={item.href}
               className={classNames(
                 item.href.toLowerCase()===currentPage.toLowerCase() ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
@@ -173,9 +175,10 @@ function Navbar() {
               aria-current={item.href.toLowerCase()===currentPage.toLowerCase() ? 'page' : undefined}
             >
               {item.name}
-            </a>
+            </Disclosure.Button>
             )}
           </div>
+        </Disclosure.Panel>
       </>
     )}
 
