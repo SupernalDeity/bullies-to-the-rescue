7258aed06591f2d6cd6c54aca2cd86b2fc019605
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-10-29 13:33:09 -0400

    changed file name to Dogs

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
-import Profile from './pages/Profile';
+import Dogs from './pages/Dogs';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Apply from './pages/Apply';
@@ -55,7 +55,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/profile" element={<Profile />} />
+            <Route path="/dogs" element={<Dogs />} />
             <Route path='/applications' element={<Applications />} />
           </Routes>
           </div>
